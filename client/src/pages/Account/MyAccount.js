8a7bce73b5515a9e8aaa4e5f2dea53d7e13569f4
Claire <claire.hw.cho@gmail.com>
2023-08-08 20:24:37 -0700

    test MyAccount's useQuery loading issue

--- client/src/pages/Account/MyAccount.js
+++ client/src/pages/Account/MyAccount.js
@@ -17,9 +17,9 @@ import AccordionDetails from "@mui/material/AccordionDetails";
 const MyAccount = () => {
     const { accountParam } = useParams();
 
-    const { data } = useQuery(QUERY_USER);
+    const { data: userData, loading: userLoading } = useQuery(QUERY_USER);
 
-    const user = data?.user || {};
+    const user = userData?.user || {};
 
     const navigate = useNavigate();
 
@@ -29,63 +29,59 @@ const MyAccount = () => {
         }
     }, [navigate]);
 
-    useEffect(() => {
-        if (!data) {
-            return navigate("/");
-        }
-    }, [data, navigate]);
-
     return (
         <section className="main-content-container">
             <div className="main-content-row">
                 <h3>
-                    Welcome to your account, {user.firstName}!
+                    {userLoading ? "Loading your account details..." : `Welcome to your account, ${user.firstName}!`}
                 </h3>
             </div>
             <div className="main-content-row">
-                <div className="account-wrapper">
-                    <div className="main-account-menu-wrapper">
-                        <Accordion
-                            sx={{
-                                width: "100%",
-                            }}
-                        >
-                            <AccordionSummary
-                                expandIcon={<ExpandMoreIcon />}
-                                aria-controls="panel1a-content"
-                                id="panel1a-header"
+                {!userLoading && (
+                    <div className="account-wrapper">
+                        <div className="main-account-menu-wrapper">
+                            <Accordion
                                 sx={{
                                     width: "100%",
                                 }}
                             >
-                                <p>MY ACCOUNT</p>
-                            </AccordionSummary>
-                            <AccordionDetails>
-                                <ul>
-                                    <li>
-                                        <Link to="/account/details" className="link">
-                                            Account Details
-                                        </Link>
-                                    </li>
-                                    <li>
-                                        <Link to="/account/orders" className="link">
-                                            Order History
-                                        </Link>
-                                    </li>
-                                    <li>
-                                        <Link to="/account/wishlist" className="link">
-                                            Wish List
-                                        </Link>
-                                    </li>
-                                </ul>
-                            </AccordionDetails>
-                        </Accordion>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="panel1a-content"
+                                    id="panel1a-header"
+                                    sx={{
+                                        width: "100%",
+                                    }}
+                                >
+                                    <p>MY ACCOUNT</p>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    <ul>
+                                        <li>
+                                            <Link to="/account/details" className="link">
+                                                Account Details
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link to="/account/orders" className="link">
+                                                Order History
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link to="/account/wishlist" className="link">
+                                                Wish List
+                                            </Link>
+                                        </li>
+                                    </ul>
+                                </AccordionDetails>
+                            </Accordion>
+                        </div>
+                        <div className="account-content-wrapper">
+                            {accountParam === "details" && <AccountDetail user={user} />}
+                            {accountParam === "orders" && <OrderHistory user={user} />}
+                        </div>
                     </div>
-                    <div className="account-content-wrapper">
-                        {accountParam === "details" && <AccountDetail user={user} />}
-                        {accountParam === "orders" && <OrderHistory user={user} />}
-                    </div>
-                </div>
+                )}
             </div>
         </section>
     );
